392799ff5cca81cffec650f69b0cdd09591e0ec8
as <>
2021-01-10 21:06:43 +0900

    プレイヤにカメラを追跡させる

--- Assets/MyScript/PlayerFollower.cs
+++ Assets/MyScript/PlayerFollower.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerFollower : MonoBehaviour
 {
+    public GameObject player;
+    Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //  カメラとプレイヤの距離を出す
+        offset = this.transform.position - player.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    //  Lateupdateは全てのクラスのUpdateが事項された後に実行される
+    void LateUpdate()
     {
-        
+        //Playerの位置を取得して、カメラの位置に代入する
+        this.transform.position = player.transform.position + offset;
     }
 }
