9c9359b018f96731293ffb81da7a86c532ebe8eb
as <>
2021-01-10 20:29:05 +0900

    Rgidbodyの追加・FixedUpdateに変更

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
+        //rbにRigitbodyを代入する
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    //  Fixedを付けるとフレーム数を関係なくする事ができる
+    void FixedUpdate()
     {
+        //  moveHに左右方向の動きを代入
         float moveH = Input.GetAxis("Horizontal");
+        //  movevに上下方向の動きを代入
         float moveV = Input.GetAxis("Vertical");
 
         Vector3 move = new Vector3(moveH, 0, moveV);
-        rb.AddForce(move);
+        //  変数rbにmoveを入れる
+        rb.AddForce(move * speed);
     }
 }
