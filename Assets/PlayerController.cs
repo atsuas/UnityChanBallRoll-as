7a6d899ff3b09a1ffede35f97c6cec64bcbb0b8c
as <>
2021-01-10 20:11:07 +0900

    Playerを動かす

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello,World");
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveH, 0, moveV);
+        rb.AddForce(move);
     }
 }
