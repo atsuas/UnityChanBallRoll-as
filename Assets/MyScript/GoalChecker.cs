d69d2e3d011877b6463796060054db147f00ee6c
as <>
2021-01-12 19:35:43 +0900

    ゴール時のBGMを追加・リトライボタンの実装

--- Assets/MyScript/GoalChecker.cs
+++ Assets/MyScript/GoalChecker.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalChecker : MonoBehaviour
 {
     public GameObject unityChan;
+    public AudioSource gameBgm;     //プレイ中のBGMを代入
+    public AudioSource goalBgm;     //ゴール後のBGMを代入
+    public GameObject retryButton;  //ボタンを代入するpublic変数
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        retryButton.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,5 +28,15 @@ public class GoalChecker : MonoBehaviour
         other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
         unityChan.transform.LookAt(Camera.main.transform);
         unityChan.GetComponent<Animator>().SetTrigger("Goal");
+
+        gameBgm.Stop();
+        goalBgm.Play();
+
+        retryButton.SetActive(true);
+    }
+
+    public void RetryStage()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
